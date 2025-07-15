6ab3eb0f0878620e166dc2331eca1f14f28acc1c
AayushiPansuriya <aayushipansuriya87@gmail.com>
2025-07-15 11:15:44 +0530

    one or more program completed

---
+++ index.js
@@ -0,0 +1,118 @@
+// var name="aayushi"
+// console.log("hellow!!"+name)
+
+// program-1
+
+// var n=5
+// if(n%2)
+// {
+//     console.log("odd number")
+// }
+// else
+// {
+//    console.log("even number")
+// }
+
+// program -2
+
+// var a=7
+// var b=10
+// if(a>b)
+// {
+//     console.log("a is maximum number")
+// }
+// else
+// {
+//     console.log("b is maximum number")
+// }
+
+// program-3
+
+// var n=2024
+// if(n%4)
+// {
+//     console.log(" none leap year")
+// }
+// else
+// {
+//     console.log(" leap year")
+// }
+
+// program-4
+
+// var a=5
+// var sum=0
+// for(i=1;i<=a;i++)
+// {
+//    sum+=i 
+// }
+// console.log(sum)
+
+// program-5
+
+// var a=4
+// var fac=1
+// for(i=1;i<=a;i++)
+// {
+//    fac*=i 
+// }
+// console.log(fac)
+
+// program-6
+
+// var a=3
+// for(i=1;i<=10;i++)
+// {
+//     console.log(`${a} x ${i} = ${a*i}`)
+// }
+
+// program-7
+
+// var a=322142
+// var b=a.toString().split('').reverse().join('')
+
+// console.log(b)
+
+// program-8
+
+// var a=121
+// var b=a.toString().split('').reverse().join('')
+// if(a==b)
+// {
+//     console.log("number is palindrome")
+// }
+// else{
+//     console.log("number is no palindrome")
+// }
+
+// program-9
+ 
+// var number=21
+// var prime=1
+// for(i=1;i<=number/2;i++)
+// {
+//     if(number%2==0)
+//     {
+//         prime=0
+//         break
+//     }
+    
+
+// }
+// if(prime&&number)
+// {
+//     console.log("number is prime")
+// }
+// else
+// {
+//     console.log("number is not prime")
+// }
+
+// program-10
+
+function digit(dnum)
+{
+    return String(dnum).length;
+}
+let dnum1=1234456
+console.log(digit(dnum1))
